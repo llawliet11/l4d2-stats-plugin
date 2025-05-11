1c3d44f3db369e065433088400dcf6a38d1e033e
Nghia Pham <pham.d.nghia@gmail.com>
2025-05-12 00:33:36 +0700

    Fix kills_all_specicies issue

--- website-api/routes/top.js
+++ website-api/routes/top.js
@@ -6,7 +6,7 @@ export default function(pool) {
     router.get('/users/:page?', routeCache.cacheSeconds(60), async(req,res) => {
         try {
             const MAX_RESULTS = req.query.max_results ? parseInt(req.query.max_results) || 15 : 15;
-
+    
             const selectedPage = req.params.page || 0;
             const pageNumber = (isNaN(selectedPage) || selectedPage <= 0) ? 0 : (parseInt(req.params.page) - 1);
             const offset = pageNumber * MAX_RESULTS;
@@ -20,7 +20,7 @@ export default function(pool) {
                     points as points_old,
                     (
                         (common_kills / 1000 * 0.10) +
-                        ((kills_smoker + kills_boomer + kills_hunter + kills_spitter + kills_jockey + kills_charger) * 0.25) +
+                        (kills_all_specials * 0.25) +
                         (revived_others * 0.05) +
                         (heal_others * 0.05) -
                         (survivor_incaps * 0.10) -
@@ -34,7 +34,7 @@ export default function(pool) {
                     ) as points
                     from stats_users
                     order by points desc
-                    limit ?,?`,
+                    limit ?,?`, 
                     [offset, MAX_RESULTS]
                 )
                 version = "v2"
@@ -50,17 +50,17 @@ export default function(pool) {
     })
     router.get('/stats', routeCache.cacheSeconds(43200), async(req,res) => {
         try {
-            const [deaths] = await pool.execute(`SELECT steamid,last_alias,points,survivor_deaths as value FROM \`stats_users\`
+            const [deaths] = await pool.execute(`SELECT steamid,last_alias,points,survivor_deaths as value FROM \`stats_users\`  
                 WHERE survivor_deaths > 0 ORDER BY \`stats_users\`.\`survivor_deaths\` desc, \`stats_users\`.\`points\` desc limit 10`, [])
-            const [ffDamage] = await pool.execute(`SELECT steamid,last_alias,points,survivor_ff as value FROM \`stats_users\`
+            const [ffDamage] = await pool.execute(`SELECT steamid,last_alias,points,survivor_ff as value FROM \`stats_users\`  
                 WHERE survivor_ff > 0 ORDER BY \`stats_users\`.\`survivor_ff\` desc, \`stats_users\`.\`points\` desc limit 10`, [])
-            const [healOthers] = await pool.execute(`SELECT steamid,last_alias,points,heal_others as value FROM \`stats_users\`
+            const [healOthers] = await pool.execute(`SELECT steamid,last_alias,points,heal_others as value FROM \`stats_users\`  
                 WHERE heal_others > 0 ORDER BY \`stats_users\`.\`heal_others\` desc, \`stats_users\`.\`points\` desc limit 10`, [])
-            const [revivedOthers] = await pool.execute(`SELECT steamid,last_alias,points,revived_others as value FROM \`stats_users\`
+            const [revivedOthers] = await pool.execute(`SELECT steamid,last_alias,points,revived_others as value FROM \`stats_users\`  
                 WHERE revived_others > 0 ORDER BY \`stats_users\`.\`revived_others\` desc, \`stats_users\`.\`points\` desc limit 10`, [])
-            const [survivorIncaps] = await pool.execute(`SELECT steamid,last_alias,points,survivor_incaps as value FROM \`stats_users\`
-                WHERE survivor_incaps > 0 ORDER BY \`stats_users\`.\`survivor_incaps\` desc, \`stats_users\`.\`points\` desc limit 10`, [])
-            const [clownHonks] = await pool.execute(`SELECT steamid,last_alias,points,clowns_honked as value FROM \`stats_users\`
+            const [survivorIncaps] = await pool.execute(`SELECT steamid,last_alias,points,survivor_incaps as value FROM \`stats_users\`  
+                WHERE survivor_incaps > 0 ORDER BY \`stats_users\`.\`survivor_incaps\` desc, \`stats_users\`.\`points\` desc limit 10`, []) 
+            const [clownHonks] = await pool.execute(`SELECT steamid,last_alias,points,clowns_honked as value FROM \`stats_users\`  
             WHERE clowns_honked > 0 ORDER BY \`stats_users\`.\`clowns_honked\` desc, \`stats_users\`.\`points\` desc limit 10 `, [])
             const [timesMVP] = await pool.execute(`
                 SELECT
@@ -75,7 +75,7 @@ export default function(pool) {
                 ORDER by stats_games.honks DESC
                 LIMIT 10
                 `, []
-            )
+            ) 
 
             res.json({
                 deaths,
